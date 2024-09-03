ecaf0f6cb2aaf85ba32c256c320a554fe9a41d80
FlaviaAMorgado <flavia.morgado@etec.sp.gov.br>
2024-09-03 14:59:23 -0300

    Subindo tela pedidos

--- edutico/Controllers/HomeController.cs
+++ edutico/Controllers/HomeController.cs
@@ -31,6 +31,10 @@ namespace edutico.Controllers
             return View();
         }
 
+        public IActionResult PedidosAndamentoF()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
