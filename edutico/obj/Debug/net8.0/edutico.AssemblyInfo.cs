ffe9b8ba8b995363f63aa9b2735eb43c75f2307b
rebekaamorim <rebeka.amorim@etec.sp.gov.br>
2024-09-27 18:12:59 -0300

    adicionano comentario em botao modal

--- edutico/obj/Debug/net8.0/edutico.AssemblyInfo.cs
+++ edutico/obj/Debug/net8.0/edutico.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("edutico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53585e0bfca6137dbbefea5868f1ae1efefcb56b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7e5a44fb30c42ff5f33a87537e1d7cebe04c9b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("edutico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("edutico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
