864d9df408c9189dc59db8e0e934fb70928eeaf1
FlaviaAMorgado <flavia.morgado@etec.sp.gov.br>
2024-09-29 14:20:55 -0300

    subindo tela cancelamento efetuado e modal pedido enviando 100%

--- edutico/Controllers/HomeController.cs
+++ edutico/Controllers/HomeController.cs
@@ -155,6 +155,11 @@ namespace edutico.Controllers
             return View();  
         }
 
+        public IActionResult CancelamentoEfetuado()
+        {
+            return View();
+        }
+
         //FUNCIONÁRIO
         public IActionResult ProdutosF()
         {
