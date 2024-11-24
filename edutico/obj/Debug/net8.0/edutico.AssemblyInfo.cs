d50b7c3c80286687898f6136ace283a7b566f4c4
evelynkarina44 <evelynkarina535@gmail.com>
2024-11-24 19:10:33 -0300

    Arrumando o alinhamento do filtro dos meus pedidos; ajustando o alinhamento dos botoes dos Meus Pedidos; Arrumando o campo das imagens do cadastrar produto; Subindo as fotos das bandeiras do cartao; Tirando o contorno do botao adicionar ao carrinho

--- edutico/obj/Debug/net8.0/edutico.AssemblyInfo.cs
+++ edutico/obj/Debug/net8.0/edutico.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("edutico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ff208941f3cbc1dfef07d9bfa7e685698d64de0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c748e0f5fa790eb9e2f5d9e2363485eb330009e")]
 [assembly: System.Reflection.AssemblyProductAttribute("edutico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("edutico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
